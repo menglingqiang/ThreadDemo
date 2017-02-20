fe7dbde9ffbec72b279b0155ca0fac65fe0b13ad
menglingqiang <1820026438@qq.com>
2017-02-20 16:37:57 +0800

    新添加web模块

---
+++ src/main/java/com/seckill/dto/SeckillResult.java
@@ -0,0 +1,20 @@
+package main.java.com.seckill.dto;
+
+public class SeckillResult<T> {
+
+	private T data;//返回成功数据
+	private String error;// 失败信息
+	private boolean flag;// 状态标识
+	
+	//成功返回数据
+	public SeckillResult(boolean flag,T data ) {
+		this.data = data;
+		this.flag = flag;
+	}
+	//失败返回失败原因
+	public SeckillResult( boolean flag,String error) {
+		this.error = error;
+		this.flag = flag;
+	}
+	
+}
---
+++ src/main/java/com/seckill/web/SeckillController.java
@@ -0,0 +1,104 @@
+package main.java.com.seckill.web;
+
+import java.util.Date;
+import java.util.List;
+
+import main.java.com.seckill.dto.Exposer;
+import main.java.com.seckill.dto.SeckillExecution;
+import main.java.com.seckill.dto.SeckillResult;
+import main.java.com.seckill.entity.Seckill;
+import main.java.com.seckill.exception.CloseException;
+import main.java.com.seckill.exception.RepeatException;
+import main.java.com.seckill.exception.SeckillException;
+import main.java.com.seckill.secenum.SeckillEnum;
+import main.java.com.seckill.service.SeckillService;
+
+import org.apache.ibatis.annotations.ResultMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+
+	@Autowired
+	private SeckillService seckillService;
+	
+	@RequestMapping(name="/list",method=RequestMethod.GET)
+	public String list(Model model)
+	{
+		List<Seckill> list = seckillService.queryAll();
+		model.addAttribute("list", list);
+		return "list";
+	}
+	
+	@RequestMapping(name="/{seckillId}/detail",method=RequestMethod.GET)
+	public String detail(@PathVariable("seckillId")Long seckillId,Model model)
+	{
+		if(seckillId==null)//当seckillId的类型是long的时候  ==会报错误
+			return "redirect:/seckill/list";
+		
+		Seckill seckill = seckillService.queryById(seckillId);
+		
+		if(seckill==null)
+			return "forward:/seckill/list";
+		model.addAttribute("seckill", seckill);
+		return "detail";
+	}
+	@RequestMapping(name="/{seckillId}/exposer",method=RequestMethod.POST
+			,produces={"application/json;charset=UTF-8"})
+	@ResponseBody
+	public SeckillResult<Exposer> exposer(@PathVariable("seckillId")Long seckillId)
+	{
+		try {
+			Exposer exposer = seckillService.exposeUrl(seckillId);
+			return new SeckillResult<Exposer>(true, exposer);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return new SeckillResult<Exposer>(false, e.getMessage());
+		}
+	}
+	@RequestMapping(name="/{seckillId}/exposer",method=RequestMethod.POST
+			,produces={"application/json;charset=UTF-8"})
+	@ResponseBody
+	public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId")Long seckillId,
+			@PathVariable("md5")String md5,
+			@CookieValue(value="userPhone",required=false) Long userPhone)
+	{
+		SeckillResult<SeckillExecution> result;
+		SeckillExecution seckillExecution ;
+		try {
+			seckillExecution = seckillService.executeSeckill(seckillId, userPhone, md5);
+			result = new SeckillResult<SeckillExecution>(true, seckillExecution);
+		} catch (RepeatException e) {
+			seckillExecution = new SeckillExecution(seckillId, SeckillEnum.REPEAT);
+			result = new SeckillResult<SeckillExecution>(false, seckillExecution);
+		} catch (CloseException e) {
+			seckillExecution = new SeckillExecution(seckillId, SeckillEnum.END);
+			result = new SeckillResult<SeckillExecution>(false, seckillExecution);
+			
+		} catch (SeckillException e) {
+			seckillExecution = new SeckillExecution(seckillId, SeckillEnum.ERROR_INNER);
+			result = new SeckillResult<SeckillExecution>(false, seckillExecution);
+		}
+		
+		return result;
+	}
+	@RequestMapping(name ="/time/now",method=RequestMethod.GET)
+	public SeckillResult<Long> getCurrentTime()
+	{
+		Date date = new Date();
+		return new SeckillResult<Long>(true,date.getTime());
+	}
+}
+
+
+
+
